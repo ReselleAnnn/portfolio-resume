bcf37fc485bc549dd89376b0e7a8f38a12f1ce1c
ReselleAnnn <160987290+ReselleAnnn@users.noreply.github.com>
2026-04-14 00:19:12 +0800

    fix: apply Geist font to layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-module.exports = {
+export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
